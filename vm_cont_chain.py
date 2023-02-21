40a4b25e668a9f0287c8ecb4faae348aa9c915ef
Gurdeep Barring <97579083+TheSikhAlchemist@users.noreply.github.com>
2023-02-20 21:37:48 -0800

    Update vm_cont_chain.py

--- vm_cont_chain.py
+++ vm_cont_chain.py
@@ -7,22 +7,18 @@ import time
 """This function (or "callback") will be executed when this client receives 
 a connection acknowledgement packet response from the server. """
 def on_connect(client, userdata, flags, rc):
-    print("Connected to server (i.e., broker) with result code "+str(rc))
+    print("Connected to server (i.e., broker) with result code " + str(rc))
     client.subscribe("barring/ping") 
-    client.message_callback_add("barring/ping", on_message_from_ping) 
+    client.message_callback_add("barring/ping", ping_func) 
 
 def on_message(client, userdata, msg):
-    print("Default callback - Topic: " + msg.topic + "   msg: " + str(msg.payload, "utf-8"))
+    print("Default callback - Topic: " + msg.topic + " msg: " + str(msg.payload, "utf-8"))
 
-def on_message_from_ping(client, userdata, message): 
+def ping_func(client, userdata, message): 
    print("Custom callback  - Ping: "+message.payload.decode())
-
-   message = int(message.payload.decode()) + 1
-
-   time.sleep(2)
-
-   client.publish("barring/pong", f"{message}") 
-
+   step_up = int(message.payload.decode()) + 1
+   time.sleep(3)
+   client.publish("barring/pong", f"{step_up}") 
    print("Publishing pong value")
 
 if __name__ == '__main__':
@@ -30,6 +26,6 @@ if __name__ == '__main__':
     client.on_message = on_message
     client.on_connect = on_connect
     client.connect(host="172.20.10.9", port=1883, keepalive=60) 
-    time.sleep(1)
+    time.sleep(3)
     client.loop_forever()
     
